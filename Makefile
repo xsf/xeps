xsf/xeps

commit 609a081f273d4d2b7c9e074bebc8c7b951242b54

Add build for inbox/

Makefile
@@ -12,6 +12,22 @@ JSTARGETS=$(OUTDIR)/prettify.js
 
 DO_XELATEX=cd $(OUTDIR); xelatex --interaction=nonstopmode -no-shell-escape "$(notdir $(basename $@)).tex" >/dev/null
 
+xeps=$(wildcard *.xml)
+proto_xeps=$(wildcard inbox/*.xml)
+all_xeps=$(xeps) $(proto_xeps)
+
+xep_xmls=$(patsubst %.xml,$(OUTDIR)/%.xml,$(xeps))
+proto_xep_xmls=$(patsubst %.xml,$(OUTDIR)/%.xml,$(proto_xeps))
+all_xep_xmls=$(xep_xmls) $(proto_xep_xmls)
+
+xep_htmls=$(patsubst %.xml,$(OUTDIR)/%.html,$(xeps))
+proto_xep_htmls=$(patsubst %.xml,$(OUTDIR)/%.html,$(proto_xeps))
+all_xep_htmls=$(xep_htmls) $(proto_xep_htmls)
+
+xep_pdfs=$(patsubst %.xml,$(OUTDIR)/%.pdf,$(xeps))
+xep_refs=$(patsubst xep-%.xml, $(REFSDIR)/reference.XSF.XEP-%.xml, $(xeps))
+xep_examples=$(patsubst xep-%.xml, $(EXAMPLESDIR)/%.xml, $(xeps))
+
 
 .PHONY: help
 help:
@@ -33,17 +49,30 @@ help:
 .PHONY: all
 all: html
 
+$(OUTDIR)/inbox/%: build/inbox
+build/inbox:
+	mkdir -p build/inbox
+
 .PHONY: html
-html: $(patsubst %.xml, $(OUTDIR)/%.html, $(wildcard *.xml))
+html: $(xep_htmls)
+
+.PHONY: xml
+xml: $(xep_xmls)
+
+.PHONY: inbox-html
+inbox-html: $(proto_xep_htmls)
+
+.PHONY: inbox-xml
+inbox-xml: $(proto_xep_xmls)
 
 .PHONY: pdf
-pdf: $(patsubst %.xml, $(OUTDIR)/%.pdf, $(wildcard *.xml))
+pdf: $(xep_pdfs)
 
 .PHONY: refs
-refs: $(patsubst xep-%.xml, $(REFSDIR)/reference.XSF.XEP-%.xml, $(wildcard *.xml))
+refs: $(xep_refs)
 
 .PHONY: examples
-examples: $(patsubst xep-%.xml, $(EXAMPLESDIR)/%.xml, $(wildcard *.xml))
+examples: $(xep_examples)
 
 .PHONY: xep-%
 xep-%: $(OUTDIR)/xep-%.html $(REFSDIR)/reference.XSF.XEP-%.xml $(OUTDIR)/xep-%.pdf $(EXAMPLESDIR)/%.xml;
@@ -54,19 +83,23 @@ xep-%.html: $(OUTDIR)/xep-%.html ;
 .PHONY: xep-%.pdf
 xep-%.pdf: $(OUTDIR)/xep-%.pdf ;
 
+$(all_xep_xmls): $(OUTDIR)/%.xml: %.xml
+	cp $< $@
+
 $(EXAMPLESDIR)/%.xml: xep-%.xml $(XMLDEPS) examples.xsl $(EXAMPLESDIR)
 	xsltproc --path $(CURDIR) examples.xsl "$<" > "$@" && echo "Finished building $@"
 
 $(REFSDIR)/reference.XSF.XEP-%.xml: xep-%.xml $(XMLDEPS) ref.xsl $(REFSDIR)
 	xsltproc --path $(CURDIR) ref.xsl "$<" > "$@" && echo "Finished building $@"
 
-$(OUTDIR)/%.html: %.xml $(XMLDEPS) $(HTMLDEPS)
+$(all_xep_htmls): $(OUTDIR)/%.html: %.xml $(XMLDEPS) $(HTMLDEPS)
+	mkdir -p $(OUTDIR)/inbox
 	xmllint --nonet --noout --noent --loaddtd --valid "$<"
 	# Check for non-data URIs
 	! xmllint --nonet --noout --noent --loaddtd --xpath "//img/@src[not(starts-with(., 'data:'))]" $< 2>/dev/null && true
 
 	# Actually build the HTML
-	xsltproc --path $(CURDIR) xep.xsl "$<" > "$@" && echo "Finished building $@"
+	xsltproc --path $(CURDIR) --param htmlbase "$(if $(findstring inbox,$<),'../','./')" xep.xsl "$<" > "$@" && echo "Finished building $@"
 
 $(OUTDIR)/xmpp.pdf $(OUTDIR)/xmpp-text.pdf: $(OUTDIR)
 	cp "resources/$(notdir $@)" "$@"

xep.xsl
@@ -40,6 +40,7 @@ OR OTHER DEALINGS IN THE SOFTWARE.
     <html>
       <head>
         <title>XEP-<xsl:value-of select='/xep/header/number'/>:<xsl:text> </xsl:text><xsl:value-of select='/xep/header/title' /></title>
+        <base><xsl:attribute name='href'><xsl:value-of select='$htmlbase'/></xsl:attribute></base>
         <link rel='stylesheet' type='text/css' href='xmpp.css' />
         <link href="prettify.css" type="text/css" rel="stylesheet" />
         <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />

Dockerfile
@@ -4,10 +4,11 @@
 FROM xmppxsf/xeps-base:latest
 
 ARG NCORES=1
-ARG TARGETS="html pdf"
+ARG TARGETS="html inbox-html inbox-xml pdf"
 
 COPY *.xml xep.* *.css *.xsl *.js *.xsl Makefile /src/
 COPY resources/*.pdf /src/resources/
+COPY inbox/*.xml inbox/*.ent inbox/*.dtd /src/inbox/
 
 WORKDIR /src
 RUN OUTDIR=/var/www/html/extensions/ make -j$NCORES $TARGETS

commit 238329a5366f3c53a4c724906d1235d049fc92db

Fix intra-XEP links (regression from 609a081

Makefile
@@ -7,8 +7,15 @@ XMLDEPS=xep.xsd xep.ent xep.dtd ref.xsl $(OUTDIR)
 TEXMLDEPS=xep2texml.xsl $(OUTDIR)/xmpp.pdf $(OUTDIR)/xmpp-text.pdf
 XEPDIRS=. inbox
 HTMLDEPS=xep.xsl $(CSSTARGETS) $(JSTARGETS)
-CSSTARGETS=$(OUTDIR)/xmpp.css $(OUTDIR)/prettify.css
-JSTARGETS=$(OUTDIR)/prettify.js
+
+base_CSSTARGETS=xmpp.css prettify.css
+CSSTARGETS=$(addprefix $(OUTDIR)/,$(base_CSSTARGETS))
+proto_CSSTARGETS=$(addprefix $(OUTDIR)/inbox/,$(base_CSSTARGETS))
+base_JSTARGETS=prettify.js
+JSTARGETS=$(addprefix $(OUTDIR)/,$(base_JSTARGETS))
+proto_JSTARGETS=$(addprefix $(OUTDIR)/inbox/,$(base_JSTARGETS))
+
+proto_HTMLDEPS=xep.xsl $(proto_CSSTARGETS) $(proto_JSTARGETS)
 
 DO_XELATEX=cd $(OUTDIR); xelatex --interaction=nonstopmode -no-shell-escape "$(notdir $(basename $@)).tex" >/dev/null
 
@@ -94,11 +101,15 @@ $(EXAMPLESDIR)/%.xml: xep-%.xml $(XMLDEPS) examples.xsl $(EXAMPLESDIR)
 $(REFSDIR)/reference.XSF.XEP-%.xml: xep-%.xml $(XMLDEPS) ref.xsl $(REFSDIR)
 	xsltproc --path $(CURDIR) ref.xsl "$<" > "$@" && echo "Finished building $@"
 
-$(all_xep_htmls): $(OUTDIR)/%.html: %.xml $(XMLDEPS) $(HTMLDEPS)
-	# we don’t put it as a dependency to avoid a rebuild due to a timestamp
-	# change on the directory
-	mkdir -p $(OUTDIR)/inbox
+$(xep_htmls): $(OUTDIR)/xep-%.html: xep-%.xml $(XMLDEPS) $(HTMLDEPS)
+	xmllint --nonet --noout --noent --loaddtd --valid "$<"
+	# Check for non-data URIs
+	! xmllint --nonet --noout --noent --loaddtd --xpath "//img/@src[not(starts-with(., 'data:'))]" $< 2>/dev/null && true
 
+	# Actually build the HTML
+	xsltproc --path $(CURDIR) --param htmlbase "$(if $(findstring inbox,$<),'../','./')" xep.xsl "$<" > "$@" && echo "Finished building $@"
+
+$(proto_xep_htmls): $(OUTDIR)/inbox/%.html: inbox/%.xml $(XMLDEPS) $(proto_HTMLDEPS)
 	xmllint --nonet --noout --noent --loaddtd --valid "$<"
 	# Check for non-data URIs
 	! xmllint --nonet --noout --noent --loaddtd --xpath "//img/@src[not(starts-with(., 'data:'))]" $< 2>/dev/null && true
@@ -130,7 +141,13 @@ $(JSTARGETS): $(OUTDIR)
 $(CSSTARGETS): $(OUTDIR)
 	cp "$(notdir $@)" "$@"
 
-$(EXAMPLESDIR) $(REFSDIR) $(OUTDIR):
+$(proto_JSTARGETS): $(OUTDIR)/inbox
+	cp "$(notdir $@)" "$@"
+
+$(proto_CSSTARGETS): $(OUTDIR)/inbox
+	cp "$(notdir $@)" "$@"
+
+$(EXAMPLESDIR) $(REFSDIR) $(OUTDIR) $(OUTDIR)/inbox:
 	mkdir -p "$@"
 
 .PHONY: clean

xep.xsl
@@ -40,7 +40,6 @@ OR OTHER DEALINGS IN THE SOFTWARE.
     <html>
       <head>
         <title>XEP-<xsl:value-of select='/xep/header/number'/>:<xsl:text> </xsl:text><xsl:value-of select='/xep/header/title' /></title>
-        <base><xsl:attribute name='href'><xsl:value-of select='$htmlbase'/></xsl:attribute></base>
         <link rel='stylesheet' type='text/css' href='xmpp.css' />
         <link href="prettify.css" type="text/css" rel="stylesheet" />
         <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />