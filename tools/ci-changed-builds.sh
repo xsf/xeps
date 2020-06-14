bf5bace92ea6ea0ab227f27cdfce245b5969dea9
Jonas Schäfer <j.wielicki@sotecware.net>
2020-06-14 15:34:20 +0200

    ci: do not include deleted files in the changed builds

--- tools/ci-changed-builds.sh
+++ tools/ci-changed-builds.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 IFS=$'\n'
-filenames="$(git diff-tree -r --no-commit-id --name-only HEAD "$1" | ( grep -P '^(xep-[0-9]{4}|inbox/[^/]+)\.xml$' || true))"
+filenames="$(git diff-tree -r --no-commit-id --name-status "$1" HEAD | ( grep -P '^[AM]\t(xep-[0-9]{4}|inbox/[^/]+)\.xml$' || true) | cut -f2)"
 if [ -z "$filenames" ]; then
     exit 0
 fi
